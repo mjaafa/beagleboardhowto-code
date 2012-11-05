9956146bf69e8fadf7591c2731278fa3a18c5b42
Mohamed Jaafar <mohamet.jaafar@gmail.com>
2012-11-05 02:19:03 +0100

    adding basic kernel config and uboot, kernel compile

--- prepare-bbx-dev-env.sh
+++ prepare-bbx-dev-env.sh
@@ -32,21 +32,32 @@ DBG_PKGS=(    "minicom"
 
 DEFAULT_BEAGLE_BOARD_PATH=${HOME}/beagle
 CROSSTOOL="crosstool-ng-1.9.1"
+TOOLCHAIN_CFG_FL=".config"
+TFTPBOOT_CFG_FL=".boot.cmd"
+BBXM_TFTP_CFG="boot.scr"
 TOOLCHAIN_URL="http://crosstool-ng.org/download/crosstool-ng/"
 TARBALL=".tar.bz2"
 SCRIPT_HOME_DIR=$PWD
 ECHO="/bin/echo -e" # works under Linux.
 COLOR=0             # with screen, tee and friends put 1 here (i.e. no color)
 
+UBOOT="u-boot-2010.12-rc3"
+UBOOT_URL="ftp://ftp.denx.de/pub/u-boot/"
+KERNEL="linux-2.6.36.2"
+KERNEL_URL="http://www.kernel.org/pub/linux/kernel/v2.6/"
+KERNEL_CFG_FL=".kernel.config"
+MLO="MLO"
+MLO_URL="http://www.angstrom-distribution.org/demo/beagleboard/"
 ##################################  UBOOT CONFIG ENV  ##################################
-BOOT_CMD="bootm 0x82000000"
 TFTP_BOOT_UIMAGE_DWL="tftpboot 0x82000000 uImage"
-TFTP_BOOT_UIMAGE_DWL="tftpboot 0x88000000 uInitrd"
+TFTP_BOOT_UINITRD_DWL="tftpboot 0x88000000 uInitrd"
+BOOT_CMD="bootm 0x82000000"
 DEFAULT_BBXM_MAC_ADDRESS="22:22:23:54:56:44"
 DEFAULT_BBXM_IP_ADDRESS="192.168.1.253" # to improve
 #DEFAULT_SERVER_IP_ADDRESS= `for i in $(ifconfig |grep "inet addr"|awk '{print $2}'|cut -d ":" -f 2); do { if [ $i -ne "127.0.0.1" ]; then echo $i fi }; done;`
 DEFAULT_SERVER_IP_ADDRESS="192.168.1.252" # to improve
-
+DEFAULT_NETMASK="255.255.255.0"
+DEFAULT_BBXM_DVI_RESOLUTION="640x480-16@60"
 ###################################### FUNCTIONS ######################################
 # some functions for text:
 off() {
@@ -149,6 +160,24 @@ function valid_mac()
     fi
     return $stat
 }
+
+function valid_netmask()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 256 && ${ip[1]} -le 256 \
+            && ${ip[2]} -le 256 && ${ip[3]} -le 256 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
 progressfilt ()
 {
     local flag=false c count cr=$'\r' nl=$'\n'
@@ -172,16 +201,32 @@ progressfilt ()
     done
 }
 
+dwl_pkg()
+{
+  local pkg=$1
+  local pkg_url=$2
+  local pkg_type=$3
+  local stat=1
+  blue "    * Download in Progress : "
+  blue "       --> PKG : " ${pkg}
+  wget --progress=bar:force ${pkg_url}${pkg}${pkg_type} 2>&1 | progressfilt
+  if [ -f ${pkg}${pkg_type} ]; then
+  blue "    * Unpacking in Progress : "
+    pv ${pkg}${pkg_type} |tar xjf - 2> /dev/null
+    stat=$?
+  fi
+ return $stat
+
+}
+
 crosstl_dwl_install_pkg()
 {
-  blue "    * Download in Progress : "
-  wget --progress=bar:force ${TOOLCHAIN_URL}${CROSSTOOL}${TARBALL} 2>&1 | progressfilt
-  if [ -f ${CROSSTOOL}${TARBALL} ]
-  then
-  blue "    * Installing in Progress : "
-    pv ${CROSSTOOL}${TARBALL} |tar xjf -
+
+  blue "   Configure : " $CROSSTOOL
+  if dwl_pkg $CROSSTOOL $TOOLCHAIN_URL $TARBALL; then
     cd $CROSSTOOL
     echo ""
+    blue "    * Configure in Progress : "
     ./configure --prefix=/opt/beagleBoard-xM
     make
     sudo make install
@@ -189,71 +234,125 @@ crosstl_dwl_install_pkg()
     cd ..
     mkdir ct-build src
     cd ct-build
-    cp $SCRIPT_HOME_DIR/.config .
+    cp $SCRIPT_HOME_DIR/${TOOLCHAIN_CFG_FL} .
     ct-ng build
   else
     red "Failed to DWL file :" ${CROSSTOOL}${TARBALL}
   fi
 }
 
-dwl_install_crosstool()
+uboot_dwl_install_pkg()
 {
-echo "   Install Toolchain using Crosstool-NG : "
-PKG_DIR=$CROSSTOOL
-PKG_TARBALL="${PKG_DIR}.tar.bz2"
-PKG_URL="${TOOLCHAIN_URL}${PKG_TARBALL}"
-  blue "    * Download in Progress : "
-  wget --progress=bar:force $PKG_URL 2>&1 | progressfilt
-  if [ -f $PKG_TARBALL ]
-  then
-  echo "    * Installing in Progress : "
-    pv $PKG_TARBALL |tar xjf -
-    cd $PKG_DIR
-    sh -c "./configure --prefix=/opt/beagleBoard-xM"
-    sh -c "make"
-    sh -c "sudo make install"
+  blue "   Configure : " $UBOOT
+  if dwl_pkg $UBOOT $UBOOT_URL $TARBALL; then
+    cd $UBOOT
+    blue "    * Configure in Progress : "
+    echo ""
+    export CROSS_COMPILE=arm-unknown-linux-gnueabi-
+    export PATH=${INSTALL_PATH}"/x-tools/bin:$PATH"
+    make distclean
+    make omap3_beagle_config
+    make
+    sudo cp ${INSTALL_PATH}"/"${UBOOT}"/tools/mkimage" "/opt/beagleBoard-xM/bin"
+    export PATH="/opt/beagleBoard-xM/bin:$PATH"
   else
-    red "Failed to DWL file :" $PKG_TARBALL
+    red "Failed to DWL file :" ${UBOOT}${TARBALL}
   fi
+
 }
 
+kernel_dwl_install_pkg()
+{
+  blue "   Configure : " $KERNEL
+  if dwl_pkg $KERNEL $KERNEL_URL $TARBALL; then
+    cd $KERNEL
+    blue "    * Configure in Progress : "
+    echo ""
+    cp ${SCRIPT_HOME_DIR}"/"${KERNEL_CFG_FL} ".config"
+    make ARCH=arm menuconfig
+    export CROSS_COMPILE=arm-unknown-linux-gnueabi-
+    export PATH=${INSTALL_PATH}"/x-tools/bin:$PATH"
+    make -j3 ARCH=arm uImage
+    cp ${INSTALL_PATH}"/"${KERNEL}"/"arch/arm/boot/uImage ${INSTALL_PATH}
+  else
+    red "Failed to DWL file :" ${KERNEL}${TARBALL}
+  fi
+
+}
 cfg_BBxM_uboot()
 {
   echo "   Configure UBOOT : "
   echo "     * Configure TFTPBOOT: "
+
   echo "       --> Enter BBxM MAC_ADDRESS :"
   read BBXM_MAC_ADDRESS
   if [ -z "$BBXM_MAC_ADDRESS" ]; then
     orange "       ==> No mac address ? using default :" $DEFAULT_BBXM_MAC_ADDRESS
+    BBXM_MAC_ADDRESS=$DEFAULT_BBXM_MAC_ADDRESS
   else
      if valid_mac $BBXM_MAC_ADDRESS; then
        green "       ==> using mac address :" $BBXM_MAC_ADDRESS
      else
        red "       ==> Wrong mac address ? using default :" $DEFAULT_BBXM_MAC_ADDRESS
+       BBXM_MAC_ADDRESS=$DEFAULT_BBXM_MAC_ADDRESS
      fi
   fi
+
   echo "       --> Enter BBxM IP_ADDRESS :"
   read BBXM_IP_ADDRESS
   if [ -z "$BBXM_IP_ADDRESS" ]; then
     orange "       ==> No ip address ? using default :" $DEFAULT_BBXM_IP_ADDRESS
+    BBXM_IP_ADDRESS=$DEFAULT_BBXM_IP_ADDRESS
   else
     if valid_ip $BBXM_IP_ADDRESS; then
        green "       ==> using ip address :" $BBXM_IP_ADDRESS
      else
        red "       ==> Wrong ip address ? using default :" $DEFAULT_BBXM_IP_ADDRESS
+       BBXM_IP_ADDRESS=$DEFAULT_BBXM_IP_ADDRESS
      fi
   fi
+
   echo "       --> Enter SERVER IP_ADDRESS :"
   read SERVER_IP_ADDRESS
   if [ -z "$SERVER_IP_ADDRESS" ]; then
     orange "       ==> No ip address ? using default :" $DEFAULT_SERVER_IP_ADDRESS
+    SERVER_IP_ADDRESS=$DEFAULT_SERVER_IP_ADDRESS
   else
     if valid_ip $SERVER_IP_ADDRESS; then
        green "       ==> using ip address :" $SERVER_IP_ADDRESS
      else
        red "       ==> Wrong ip address ? using default :" $DEFAULT_SERVER_IP_ADDRESS
+       SERVER_IP_ADDRESS=$DEFAULT_SERVER_IP_ADDRESS
      fi
   fi
+
+  echo "       --> Enter NETWORK_MASK      :"
+  read NETWORK_MASK
+  if [ -z "$NETWORK_MASK" ]; then
+    orange "       ==> No network mask? using default :" $DEFAULT_NETMASK
+    NETWORK_MASK=$DEFAULT_NETMASK
+  else
+    if valid_netmask $NETWORK_MASK; then
+       green "       ==> using network mask :" $NETWORK_MASK
+     else
+       red "       ==> Wrong ip network mask ? using default :" $DEFAULT_NETMASK
+       NETWORK_MASK=$DEFAULT_NETMASK
+     fi
+  fi
+
+  cp ${SCRIPT_HOME_DIR}"/"${TFTPBOOT_CFG_FL} ${INSTALL_PATH}
+  echo "       --> Patching BeagleBoardxM TFTPBOOT environnement"
+  echo "           ==> Warning the DVI mode supported is 640x480-16@60"
+  sed -i "s/BBXM_MAC_ADDRESS/$(echo $BBXM_MAC_ADDRESS |sed 's/\//\\\//g')/g" ${TFTPBOOT_CFG_FL}
+  sed -i "s/SERVER_IP/$(echo $SERVER_IP_ADDRESS |sed 's/\//\\\//g')/g" ${TFTPBOOT_CFG_FL}
+  sed -i "s/BBXM_IP/$(echo $BBXM_IP_ADDRESS |sed 's/\//\\\//g')/g" ${TFTPBOOT_CFG_FL}
+  sed -i "s/NETMASK/$(echo $NETWORK_MASK |sed 's/\//\\\//g')/g" ${TFTPBOOT_CFG_FL}
+  sed -i "s/BBXM_DVI_RESOLUTION/$(echo $DEFAULT_BBXM_DVI_RESOLUTION |sed 's/\//\\\//g')/g" ${TFTPBOOT_CFG_FL}
+  echo $TFTP_BOOT_UIMAGE_DWL  >> ${TFTPBOOT_CFG_FL}
+  echo $TFTP_BOOT_UINITRD_DWL >> ${TFTPBOOT_CFG_FL}
+  echo $BOOT_CMD              >> ${TFTPBOOT_CFG_FL}
+  mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n 'Boot script' -d ${TFTPBOOT_CFG_FL} ${BBXM_TFTP_CFG}
+  dwl_pkg $MLO
 }
 
 ##################################   SCRIPT START     ##################################
@@ -267,11 +366,11 @@ yellow "   Install path preference : "
 chk_parms $# $1
 
 
-sed -i "s/INSTALL_PATH/$(echo $INSTALL_PATH |sed 's/\//\\\//g')/g" ".config"
+sed -i "s/INSTALL_PATH/$(echo $INSTALL_PATH |sed 's/\//\\\//g')/g" ${TOOLCHAIN_CFG_FL}
 
 blue ""
 blue "   Install necessary packages for compilation : "
-install_comp_pkg CMP_PKGS
+##install_comp_pkg CMP_PKGS
 
 blue ""
 blue "   Install necessary packages for debug : "
@@ -281,8 +380,18 @@ echo ""
 mkdir -p $INSTALL_PATH
 cd $INSTALL_PATH
 
+#TOOLCHAIN_CFG
 crosstl_dwl_install_pkg
-PKG_DIR=$CROSSTOOL
-PKG_TARBALL="${PKG_DIR}.tar.bz2"
-PKG_URL="${TOOLCHAIN_URL}${PKG_TARBALL}"
+cd $INSTALL_PATH
 
+#UBOOT_CFG
+uboot_dwl_install_pkg
+cd $INSTALL_PATH
+
+#UBOOT_ENV
+cfg_BBxM_uboot
+cd $INSTALL_PATH
+
+#KERNEL_CFG
+kernel_dwl_install_pkg
+cd $INSTALL_PATH
